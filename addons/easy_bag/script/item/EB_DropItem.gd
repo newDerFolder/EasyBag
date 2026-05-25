56df70b862eebf978c5287859e40af18557ad7d6
newDerFolder <KLderMR@163.com>
2026-05-25 20:21:29 +0800

    drop item func

--- addons/easy_bag/script/item/EB_DropItem.gd
+++ addons/easy_bag/script/item/EB_DropItem.gd
@@ -4,3 +4,10 @@ class_name EB_DropItem extends EB_BaseItem
 @export var drop_weight:int=1
 @export var item:EB_InventoryItem
 @export var attribute_arr:Array[EB_BaseAttribute]
+
+func create_instance()->EB_InventoryItem:
+	var drop_item=item.clone_self()
+	for i in attribute_arr:
+		if drop_item.get_attribute_by_name(i.attribute_name):
+			drop_item.get_attribute_by_name(i.attribute_name).set_value(i.get_value())
+	return drop_item
