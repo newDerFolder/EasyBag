42a68aa5c9192763e39311c5255d45e3d21501a6
newDerFolder <KLderMR@163.com>
2026-06-07 10:12:29 +0800

    dev

--- addons/easy_bag/script/attribute/EB_BaseAttributeOverride.gd
+++ addons/easy_bag/script/attribute/EB_BaseAttributeOverride.gd
@@ -3,7 +3,12 @@ class_name EB_BaseAttributeOverride extends EB_BaseAttribute
 @export var attribute:EB_BaseAttribute
 
 #TODO:做重新属性数值类中...
-
+#enum Hand_Mode{
+	#ExactMatch,#完全一致
+	#CompareNumber,#大于等于即可
+	#CheckAndConsumeNumber#大于等于并且减少
+#}
+#@export var hand_mode:Hand_Mode=Hand_Mode.ExactMatch
 func get_value():
 	return attribute.get_value()
 func set_value(new_value):
