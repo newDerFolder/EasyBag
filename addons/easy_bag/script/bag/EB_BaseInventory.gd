b4e90b09d85251acd91f433ca13dc30d25d2d064
NewDerFolder <KLderMR@163.com>
2026-07-18 17:55:32 +0800

    abs

--- addons/easy_bag/script/bag/EB_BaseInventory.gd
+++ addons/easy_bag/script/bag/EB_BaseInventory.gd
@@ -3,3 +3,7 @@
 
 #@abstract func add_item(item:EB_InventoryItem)
 #@abstract func get_all_items()->Array[EB_InventoryItem]
+
+@abstract func move_item_to_inventory(to_inventory:EB_BaseInventory,item:EB_InventoryItem)->bool
+
+@abstract func receive_item_from_inventory(item:EB_InventoryItem) -> bool
