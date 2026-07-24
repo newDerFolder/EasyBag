bb80b2bbbfa0cb4ee22316ccf521d3faa99e3e12
NewDerFolder <KLderMR@163.com>
2026-07-24 19:37:46 +0800

    drop

--- addons/easy_bag/script/item/EB_DropItem.gd
+++ addons/easy_bag/script/item/EB_DropItem.gd
@@ -4,7 +4,7 @@ class_name EB_DropItem extends EB_BaseItem
 ## 掉落项,通常放在 EB_DropPool 中
 
 @export var drop_weight:int=1
-@export var item:EB_InventoryItemOverride
+@export var item:EB_InventoryItem
 
 func create_instance()->EB_InventoryItem:
-	return item.create_instance()
+	return item.clone_self()
